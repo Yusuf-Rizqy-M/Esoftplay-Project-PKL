4ec235cda8df6b6a5177fc32dd166361cd6ba402
Yusuf-Rizqy-M <yusufhabib290@gmail.com>
2025-12-10 13:40:52 +0700

    revised 2

--- modules/interns/admin/interns_tasks_list.php
+++ modules/interns/admin/interns_tasks_list.php
@@ -1,95 +1,53 @@
 <?php if (!defined('_VALID_BBC')) exit('No direct script access allowed');
 
-/* ===========================
-   SEARCH
-   =========================== */
 $formSearch = _lib('pea', 'interns_tasks_list');
 $formSearch->initSearch();
 
-$formSearch->search->addInput('keyword','keyword');
-$formSearch->search->input->keyword->addSearchField('notes', true);
+$formSearch->search->addInput('keyword', 'keyword');
+$formSearch->search->input->keyword->addSearchField('notes', false);
 
 $add_sql = $formSearch->search->action();
 echo $formSearch->search->getForm();
 
-/* ===========================
-   TABS
-   =========================== */
+
 $tabs = array(
-  'Tasks'     => '',
-  'Add Task'  => ''
+  'Tasks'    => '',
+  'Add Task' => ''
 );
 
-/* ===========================
-   FORM ADD / EDIT
-   =========================== */
-$formAdd = _lib('pea', 'interns_tasks_list');
-$formAdd->initEdit();
-
-$formAdd->edit->addInput('header','header');
-$formAdd->edit->input->header->setTitle('Add / Edit Intern Task');
-
-/* INTERN PICKER */
-$formAdd->edit->addInput('interns_id','selecttable');
-$formAdd->edit->input->interns_id->setTitle('Intern');
-$formAdd->edit->input->interns_id->setModal();
-$formAdd->edit->input->interns_id->setReferenceTable('interns');
-$formAdd->edit->input->interns_id->setReferenceField('name', 'id');
-$formAdd->edit->input->interns_id->setRequire();
-
-/* TASK MASTER PICKER */
-$formAdd->edit->addInput('interns_tasks_id','selecttable');
-$formAdd->edit->input->interns_tasks_id->setTitle('Task');
-$formAdd->edit->input->interns_tasks_id->setModal();
-$formAdd->edit->input->interns_tasks_id->setReferenceTable('interns_tasks');
-$formAdd->edit->input->interns_tasks_id->setReferenceField('title','id');
-$formAdd->edit->input->interns_tasks_id->setRequire();
-
-/* NOTES */
-$formAdd->edit->addInput('notes','textarea');
-$formAdd->edit->input->notes->setTitle('Notes');
-
-/* STATUS */
-$formAdd->edit->addInput('status','select');
-$formAdd->edit->input->status->setTitle('Status');
-$formAdd->edit->input->status->addOption('To Do', 1);
-$formAdd->edit->input->status->addOption('In Progress', 2);
-$formAdd->edit->input->status->addOption('Submit', 3);
-$formAdd->edit->input->status->addOption('Revised', 4);
-$formAdd->edit->input->status->addOption('Done', 5);
-$formAdd->edit->input->status->addOption('Cancel', 6);
-$formAdd->edit->input->status->setRequire();
-
-$formAdd->edit->action();
+include 'interns_tasks_list_edit.php';
 $tabs['Add Task'] = $formAdd->edit->getForm();
 
-/* ===========================
-   LIST TABLE
-   =========================== */
+
 $formList = _lib('pea', 'interns_tasks_list');
 $formList->initRoll($add_sql.' ORDER BY id DESC', 'id');
 
-/* HIDDEN ID */
+/* DISABLE DELETE & SAVE */
+$formList->roll->setDeleteTool(false);
+$formList->roll->setSaveTool(true);
+
+// id
 $formList->roll->addInput('id','sqlplaintext');
 $formList->roll->input->id->setDisplayColumn(false);
 
-/* INTERN COLUMN */
 $formList->roll->addInput('interns_id','selecttable');
 $formList->roll->input->interns_id->setTitle('Intern');
+$formList->roll->input->interns_id->setPlaintext(true);
 $formList->roll->input->interns_id->setReferenceTable('interns');
 $formList->roll->input->interns_id->setReferenceField('name','id');
 
-/* TASK MASTER COLUMN */
-$formList->roll->addInput('interns_tasks_id','selecttable');
+$formList->roll->addInput('interns_tasks_id','selecttable','sqlplaintext');
 $formList->roll->input->interns_tasks_id->setTitle('Task');
+$formList->roll->input->interns_tasks_id->setPlaintext(true);
 $formList->roll->input->interns_tasks_id->setReferenceTable('interns_tasks');
 $formList->roll->input->interns_tasks_id->setReferenceField('title','id');
 
-/* NOTES */
+
+// notes
 $formList->roll->addInput('notes','sqlplaintext');
 $formList->roll->input->notes->setTitle('Notes');
 
-/* STATUS (READABLE) */
+// status
 $formList->roll->addInput('status','select');
 $formList->roll->input->status->setTitle('Status');
 $formList->roll->input->status->addOption('To Do', 1);
@@ -99,18 +57,17 @@ $formList->roll->input->status->addOption('Revised', 4);
 $formList->roll->input->status->addOption('Done', 5);
 $formList->roll->input->status->addOption('Cancel', 6);
 
-/* CREATED */
+// created
 $formList->roll->addInput('created','sqlplaintext');
 $formList->roll->input->created->setTitle('Created');
 
-/* UPDATED */
+// updated
 $formList->roll->addInput('updated','sqlplaintext');
 $formList->roll->input->updated->setTitle('Updated');
 
 $formList->roll->action();
-$formList->roll->onDelete(true);
 
 $tabs['Tasks'] = $formList->roll->getForm();
 
-/* SHOW TABS */
+// show tabs
 echo tabs($tabs, 1, 'tabs_interns_tasks_list');
---
+++ modules/interns/admin/interns_tasks_list_history.php
@@ -0,0 +1,40 @@
+<?php if (!defined('_VALID_BBC')) exit('No direct script access allowed');
+
+$form = _lib('pea', 'interns_tasks_list_history');
+
+/* SEARCH */
+$form->initSearch();
+$form->search->addInput('keyword', 'keyword');
+$form->search->input->keyword->addSearchField('status', true);
+
+$add_sql = $form->search->action();
+echo $form->search->getForm();
+
+/* LIST HISTORY */
+$form->initRoll($add_sql.' ORDER BY id DESC', 'id');
+
+/* No Save/Delete */
+$form->roll->setDeleteTool(false);
+$form->roll->setSaveTool(false);
+
+/* ID */
+$form->roll->addInput('id', 'sqlplaintext');
+$form->roll->input->id->setDisplayColumn(false);
+
+/* INTERN TASKS LIST (join ke tasks) */
+$form->roll->addInput('interns_tasks_list_id', 'selecttable');
+$form->roll->input->interns_tasks_list_id->setTitle('Task');
+$form->roll->input->interns_tasks_list_id->setPlaintext(true);
+$form->roll->input->interns_tasks_list_id->setReferenceTable('interns_tasks_list');
+$form->roll->input->interns_tasks_list_id->setReferenceField('notes','id');
+
+/* STATUS */
+$form->roll->addInput('status', 'sqlplaintext');
+$form->roll->input->status->setTitle('Status');
+
+/* CREATED */
+$form->roll->addInput('created', 'sqlplaintext');
+$form->roll->input->created->setTitle('Created');
+
+/* OUTPUT */
+echo $form->roll->getForm();
